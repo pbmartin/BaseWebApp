cdef70c0ff35bb27bc924facd577b0f3e824e773
Paul Martin <paul.martin@dickersonbakker.com>
2020-05-25 11:55:00 -0500

    add weather again

--- project/start.js
+++ project/start.js
@@ -21,6 +21,10 @@ app.get('/projects', function(request, response) {
   response.render('pages/projects');
 });
 
+app.get('/weather', function(request, response) {
+  response.render('pages/weather');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
